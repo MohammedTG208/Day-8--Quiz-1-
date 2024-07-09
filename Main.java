fded398b42986bd20dd06674b6887bd878bb27e5
Mohammed Tariq <mooode208@gmail.com>
2024-07-09 11:44:53 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+//        1.Write a Java program that accept three numbers from the user and print the largest number .
+//        System.out.println("Enter 1st number");
+//        int num1=input.nextInt();
+//        System.out.println("Enter 2nd number: ");
+//        int num2= input.nextInt();
+//        System.out.println("Enter 3th number ");
+//        int num3= input.nextInt();
+//        if (num1>num2&&num1>num3){
+//            System.out.println("the largest number "+num1);
+//        } else if (num2>num1&&num2>num3) {
+//            System.out.println("the largest number "+num2);
+//        } else if (num3>num1&&num3>num2) {
+//            System.out.println("the largest number "+num3);
+//        }else {
+//            System.out.println("Enter again");
+//        }
+//        2.Write a Java program that accept a String and a number from the user,then print the character in the given index .
+//        System.out.println("Enter Text");
+//        String text=input.nextLine();
+//        System.out.println("Enter Integer");
+//        int number= input.nextInt();
+//        System.out.println(text.charAt(number));
+//        3.Write a program to enter the numbers till the user wants and at the end it should display the sum entered .
+//        int number=0;
+//        String  isY="";
+//        int sum=0;
+//        do {
+//            System.out.println("Enter number");
+//            number= input.nextInt();
+//            System.out.println("Do yo want to complete?? Y/N");
+//            isY=input.next().toLowerCase();
+//            sum+=number;
+//        }while (!isY.equalsIgnoreCase("n"));
+//        System.out.println("The sum for all numbers : "+sum);
+//        4.Write a Java program to find positive and negative numbers of a given array:
+//        -  Original Array:
+//      [10, -21 , 30, 31, -25]
+//        -  Expected Output:
+//        10  is a positive number
+//        -21 is a negative number
+//        30 is a positive number
+//        31 is a positive number
+//        -25 is a negative number
+//        int[] myArray={10, -21 , 30, 31, -25};
+//        for (int num:myArray){
+//            if(num>=0){
+//                System.out.println(num+" is a positive number");
+//            } else{
+//                System.out.println(num+" is a negative number");
+//            }
+//        }
+//        5.Write a Java program to find a shortest word of a given array:
+//        - Original Array:
+//    [“Tuwaiq”, “Bootcamp” , “Student” , ”JAVA”]
+//        - Expected Output:
+//        JAVA
+//        ArrayList<String> language=new ArrayList<>();
+//        language.add("Bootcamp");
+//        language.add("Tuwaiq");
+//        language.add("Student");
+//        language.add("JAVA");
+//        String text1="";
+//        ArrayList<String> lang=new ArrayList<>();
+//        for(int i=0; i < language.size();i++){
+//            for (int j = i; j < language.size() ; j++) {
+//                System.out.println(language.get(j+1));
+//                if (language.get(i).length()<language.get(j).length()){
+//
+//                }
+//            }
+//        }
+//        System.out.println(text1);
+    }
+}
